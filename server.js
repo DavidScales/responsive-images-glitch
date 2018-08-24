d72f164fdcdb0704521fa352367af64ab859b831
David Scales <davidsscales@gmail.com>
2018-08-24 14:49:30 -0700

    feat: link to solution with server route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/solution', function(request, response) {
+  response.sendFile(__dirname + '/views/solution.html');
+});
+
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
